8a05e251dd5069b1441f06dc5641987617e449e9
zim0369 <zim@onionmail.org>
2022-02-02 18:35:15 +0530

    updated install and uninstall scripts

---
+++ sample.sh
@@ -0,0 +1,2 @@
+printf '%s\n' "    ${BLUE}[${RED}*${BLUE}] ${ORANGE}Uninstalling pretty-kitty...";
+printf '%s\n' "    ${BLUE}[${RED}*${BLUE}] ${RED}Removing files from '/usr/local/share' dir.${BLUE}";
